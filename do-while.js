c8e0f07cd12f837f55641a2017521ca998f9df5a
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-01-30 16:25:46 +0600

    do-while loop. minimum ek bar cholbe

---
+++ do-while.js
@@ -0,0 +1,17 @@
+/**
+ * initialization;
+ * 
+ * do {
+ *      .. statement ..
+ * 
+ * } while(condition);
+ * 
+ * ==> minimun 1 bar execute hobe
+ */
+
+let i = 10;
+
+do {
+    console.log(i);
+    i++;
+} while(i < 5);
