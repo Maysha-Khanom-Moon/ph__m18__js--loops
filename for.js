0adbef5ed28fc19735fda83779df731e2c5c0483
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-01-30 15:34:42 +0600

    for loop

---
+++ for.js
@@ -0,0 +1,21 @@
+/**
+ * for(initial, update, checking) {
+ *      .. statement ..
+ * }
+ */
+
+for(let num = 0; num < 5; num++) {
+    console.log(num);
+}
+
+// odd
+console.log('\nOdd:');
+for(let i = 1; i < 10; i += 2) {
+    console.log(i);
+}
+
+// even 
+console.log('\nEven:');
+for(let i = 2; i <= 10; i+=2) {
+    console.log(i);
+}
