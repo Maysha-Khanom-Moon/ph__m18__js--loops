c3aed082fcd1dc13388f557f8865da696159f167
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-01-30 14:49:05 +0600

    for-of loop to access every element serial wise

---
+++ for-of.js
@@ -0,0 +1,14 @@
+const numbers = [12, 98, 45, 63, 21, 72];
+
+console.log(numbers);
+
+
+/**
+ * ---------- for-of -----------
+ * 
+ * ==> serial wise element gulo get kore
+ */
+
+for(const nums of numbers) {
+    console.log(nums);
+}
