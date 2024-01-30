d530f2c913007aaab0f59b438e50b83303a4cf3d
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-01-30 15:15:08 +0600

    while loop

---
+++ while.js
@@ -0,0 +1,25 @@
+let num = 7;
+
+if(num < 1) {
+    console.log('choto ekta number paisi');
+}
+
+console.log('something new is cooking');
+
+// ---------- while ----------
+
+let number = 0;
+while (number < 5) {
+    console.log('loooooooping', number);
+
+    number++;
+}
+
+
+// 1 to 10
+num = 1;
+while(num <= 10) {
+    console.log(num);
+
+    num++;
+}
